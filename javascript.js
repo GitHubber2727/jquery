00ba212321bb9881e3590d7a6f34fff9f44f1276
Manda <mandymoran27@yahoo.com>
2017-10-31 08:19:33 -0400

    jquerylab

---
+++ javascript.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+$(function(){
+$(".available").click(function() {
+$("#form").show();    
+});
+});
+  
+$(function(){
+$('#xbox').click(function(){
+$('#form').hide();  
+});
+});
+ 
+$(function(){    
+$("#reserve").click(function() {
+$('#form').hide();
+
+});
+});
+
+
+    
+});
