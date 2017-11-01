abb37eb381c4c649e1cdbd30f11a08a09806c4ca
Manda <mandymoran27@yahoo.com>
2017-11-01 17:11:22 -0400

    updated jquery lab

--- javascript.js
+++ javascript.js
@@ -1,10 +1,22 @@
-$(document).ready(function() {
 
-$(function(){
-$(".available").click(function() {
-$("#form").show();    
+
+$(document).ready(function() {
+    
+var tableInfo = [null, null, null, null, null, null, null, null, null]
+
+
+
+
+var currentTable = 0;
+
+$(".available").click(function(){
+$("#form").show();
+currentTable = $(this).data("tablenum");
+$("#tableNumber").text("Table Number: " + currentTable);
 });
-});
+
+    
+
   
 $(function(){
 $('#xbox').click(function(){
@@ -22,3 +34,48 @@ $('#form').hide();
 
     
 });
+
+// function getNumberFromTable(table) {
+// return parseInt ( $(table).text().trim() );
+
+// $("available").on("click", function(){
+// $("#reservation").css("display", "block"
+// lastTableClicked = this;
+ // var tableNum = $(this).text();
+// $("#tableNumber").text("Table Number: " + tableNum);
+
+
+// div id= tableNumber 
+
+ // $("saveButton").on("click", function () {
+
+  
+  // 1. add reserved class to the table
+  //   $(lastTableClicked).addClass("reserved").removeClass("available");
+     
+  // 2. store the info from form
+// var name = $("#name").val();
+ //    var partySize = $("#partsySize).val();
+ //   var reservation = {
+ //                      name: name,
+//                       partySize: partySize
+ //                      };
+//                       console.log(reservation);
+// var tableNum = getNumberFromTable(lastTableClicked)
+// tableReservations[tableNum] = reservation;
+  // 3. hide the form
+// $('#reservation").css("display", "none");
+// });
+
+// how to mouseover effect on a button...
+// $('.available').on('mouseover', function() {
+ //   $(this).toggle 
+/ }
+                   
+// how to add hover on tables clicked
+                   // delegated events
+                   // $(".table").on("mouseenter", function() {
+ //                  var tableNum = getNumberFromTable(this);
+ //                   if ($(this).hasClass("reserved")) {
+ //                 var reservation = tableReservations [tableNum]
+    //               })
